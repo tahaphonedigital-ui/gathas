4e0481d14a85df88bb729168023fc85a8787ff36
tahaphonedigital-ui <tahaphonedigital@gmail.com>
2026-07-25 07:54:28 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// ظاهر شدن کارت‌ها هنگام اسکرول
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.style.opacity="1";
+            entry.target.style.transform="translateY(0)";
+        }
+    });
+});
+
+cards.forEach(card=>{
+    card.style.opacity="0";
+    card.style.transform="translateY(60px)";
+    card.style.transition="all .8s ease";
+    observer.observe(card);
+});
